b369af8a6eca68bf5b680a89ac0da16c1d095f61
Damir Yakupov <yakupovdamir93@gmail.com>
2022-07-27 22:49:45 -0400

    add take

---
+++ take/tests/take.test.js
@@ -0,0 +1,12 @@
+import { strict as assert } from 'assert';
+import getFunction from '../functions.js';
+
+const take = getFunction();
+
+// BEGIN (write your solution here)
+assert.deepStrictEqual(take(['one', 'two', 'three'], 2), ['one', 'two']);
+assert.deepStrictEqual(take(['one', 'two', 8], 9), ['one', 'two', 8]);
+assert.deepStrictEqual(take([1, 2]), [1]);
+assert.deepStrictEqual(take([]), []);
+assert.deepStrictEqual(take([4, 3], -1), []);
+// END
