1595f78d0da96e2ca80c9d3c3f04990caab9b724
Damir Yakupov <yakupovdamir93@gmail.com>
2022-07-31 21:58:37 -0400

    add test

--- indexOf/tests/indexOf.test.js
+++ indexOf/tests/indexOf.test.js
@@ -6,9 +6,8 @@ import getFunction from '../functions.js';
 
 const indexOf = getFunction();
 
-// BEGIN (write your solution here)
+
 assert(indexOf([1, 2, 1, 2], 2) === 1);
 assert(indexOf([], 9) === -1);
 assert(indexOf(['one', 'two', 8], 'one') === 0);
 assert(indexOf([1, 2, 1, 2], 2, 2) === 3);
-// END
